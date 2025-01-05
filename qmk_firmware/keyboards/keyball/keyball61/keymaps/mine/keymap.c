965290090c4b26ec8ade088a73bcde89b32b28f9
Banana-Tachi <tachikoki@gmail.com>
2025-01-05 21:30:57 +0900

    右手側トラボレイヤキー位置変更

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mine/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mine/keymap.c
@@ -70,8 +70,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_GRV , KC_1   , KC_2   , KC_3        , KC_4       , KC_5  ,                               KC_6   , KC_7   , KC_8   , KC_9   , KC_0         , KC_EQL ,
     KC_TAB , KC_Q   , KC_W   , KC_E        , KC_R       , KC_T  ,                               KC_Y   , KC_U   , KC_I   , KC_O   , KC_P         , KC_MINS,
-    KC_LSFT, KC_A   , KC_S   , KC_D        , KC_F       , KC_G  ,                               KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN      , KC_QUOT,
-    KC_LCTL, KC_Z   , KC_X   , KC_C        , KC_V       , KC_B  , LT(3,KC_LBRC), LT(3,KC_RBRC), KC_N   , KC_M   , KC_COMM, KC_DOT , LT(2,KC_SLSH), KC_RSFT,
+    KC_LSFT, KC_A   , KC_S   , KC_D        , KC_F       , KC_G  ,                               KC_H   , KC_J   , KC_K   , KC_L   , LT(2,KC_SCLN), KC_QUOT,
+    KC_LCTL, KC_Z   , KC_X   , KC_C        , KC_V       , KC_B  , LT(3,KC_LBRC), LT(3,KC_RBRC), KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH      , KC_RSFT,
     KC_LCTL, KC_LWIN, KC_LALT, LT(1,KC_ESC), LT_LCTL_SPC, KC_SPC, MO(2)        , KC_ENT       , KC_BSPC, KC_NO  , KC_NO  , KC_NO  , LT(1,KC_BSLS), KC_RCTL
   ),
 
@@ -86,8 +86,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [2] = LAYOUT_universal(
     _______, KC_1   , KC_2   , KC_3        , KC_4   , KC_5   ,                                KC_6      , KC_7      , KC_8    , KC_9    , KC_0    , KC_EQL ,
     _______, KC_Q   , KC_W   , KC_BRID     , KC_BRIU, KC_T   ,                                CPI_D1K   , CPI_D100  , CPI_I100, CPI_I1K , SCRL_DVD, KC_MINS,
-    _______, _______, KC_MUTE, KC_VOLD     , KC_VOLU, KC_G   ,                                AML_TO    , KC_BTN1   , SCRL_MO , KC_BTN2 , SCRL_DVI, _______,
-    _______, _______, KC_X   , KC_C        , KC_V   , KC_B   , LT(3,KC_LBRC), LT(3,KC_RBRC) , A(KC_RGHT), A(KC_LEFT), KC_BTN3 , KC_NO   , KC_NO   , _______,
+    _______, _______, KC_MUTE, KC_VOLD     , KC_VOLU, KC_G   ,                                AML_TO    , KC_BTN1   , SCRL_MO , KC_BTN2 , KC_NO   , _______,
+    _______, _______, KC_X   , KC_C        , KC_V   , KC_B   , LT(3,KC_LBRC), LT(3,KC_RBRC) , A(KC_RGHT), A(KC_LEFT), KC_BTN3 , KC_NO   , SCRL_DVI, _______,
     LLOCK  , _______, KC_LALT, LT(1,KC_ESC), _______, _______, _______      , _______       , _______   , _______   , _______ , _______ , _______ , _______
   ),
 
