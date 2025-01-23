068f4f199a004cc81863ab3a94518a560f1ae9a5
Banana-Tachi <tachikoki@gmail.com>
2025-01-24 04:58:54 +0900

    キー配置修正＆キー追加Tap/Hold: Toggle Layer3/LShit

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mine/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mine/keymap.c
@@ -27,6 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum custom_keycodes {
     LT_LCTL_SPC = SAFE_RANGE, // Tap/Hold: IME/Layer1 
     LLOCK,
+    LT_LSFT_L3, // Tap/Hold: Toggle Layer3/LShit
+    LT_RSFT_L3, // Tap/Hold: Toggle Layer3/RShit
 };
 
 static bool layer_1_active = false;
@@ -38,6 +40,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         return false;
     }
     switch (keycode) {
+        case LT_LSFT_L3:
+        case LT_RSFT_L3:
+            if (record->event.pressed) {
+                if (!layer_1_active) {  // shiftを長押し中でない場合のみ
+                    register_code(KC_LSFT);
+                }
+                lt_timer = timer_read();
+                return false;
+            } else {
+                unregister_code(KC_LSFT);
+                if (timer_elapsed(lt_timer) < TAPPING_TERM) {
+                    layer_invert(3);  // タップでレイヤー3をトグル
+                }
+                return false;
+            }
+            break;
         case LT_LCTL_SPC:
             if (record->event.pressed) {
                 // キーを押した時
@@ -71,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV , KC_1   , KC_2   , KC_3                       , KC_4       , KC_5  ,                               KC_6   , KC_7   , KC_8   , KC_9   , KC_0         , KC_EQL ,
     KC_TAB , KC_Q   , KC_W   , KC_E                       , KC_R       , KC_T  ,                               KC_Y   , KC_U   , KC_I   , KC_O   , KC_P         , KC_MINS,
     KC_LSFT, KC_A   , KC_S   , KC_D                       , KC_F       , KC_G  ,                               KC_H   , KC_J   , KC_K   , KC_L   , LT(2,KC_SCLN), KC_QUOT,
-    KC_LCTL, KC_Z   , KC_X   , KC_C                       , KC_V       , KC_B  , TT(3)        , TT(3)        , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH      , KC_RSFT,
+    KC_LCTL, KC_Z   , KC_X   , KC_C                       , KC_V       , KC_B  , TT(3)        , KC_RSFT      , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH      , KC_RSFT,
     KC_LCTL, KC_LWIN, KC_LALT,MT(MOD_LCTL|MOD_LSFT,KC_ESC), LT_LCTL_SPC, KC_SPC, TT(2)        , KC_ENT       , KC_BSPC, KC_NO  , KC_NO  , KC_NO  , LT(1,KC_BSLS), KC_RCTL
   ),
 
@@ -87,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_6      , KC_7      , KC_8    , KC_9    , KC_0    , KC_EQL ,
     _______, KC_WREF, KC_WSCH, KC_BRID, KC_BRIU, KC_MNXT,                   CPI_D100  , CPI_I100  , SCRL_TO   , _______ , SCRL_DVD, KC_MINS,
     _______, C(KC_A), KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY,                   AML_TO    , KC_BTN1   , SCRL_MO , KC_BTN2 , KC_NO   , _______,
-    _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_MPRV, _______, TT(3)  , A(KC_RGHT), A(KC_LEFT), KC_BTN3 , KC_NO   , SCRL_DVI, _______,
+    _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), KC_MPRV, _______, KC_RSFT, A(KC_RGHT), A(KC_LEFT), KC_BTN3 , KC_NO   , SCRL_DVI, _______,
     LLOCK  , _______, _______, _______, _______, _______, _______, _______, _______   , _______   , _______ , _______ , _______ , _______
   ),
 
